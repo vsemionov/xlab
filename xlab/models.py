4388bbb3e48ca63b97167a8f4b49c5853ae2344d
Victor Semionov <vsemionov@gmail.com>
2025-03-31 18:42:46 +0300

    lightning module

--- xlab/models.py
+++ xlab/models.py
@@ -18,9 +18,14 @@
 # https://github.com/karpathy/minGPT
 # https://github.com/pytorch/pytorch/blob/main/torch/nn/modules/transformer.py
 
+from typing import Optional, Callable
+
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import lightning as L
 
 
 class PositionalEncoding(nn.Module):
@@ -203,4 +208,58 @@ class GenerativeTextTransformer(nn.Module):
         x = self.embedding(x) * math.sqrt(self.embedding.embedding_dim)
         x = self.transformer(x, seq_mask=pad_mask)
         y = self.linear(x)
+        return y
+
+
+class XLabTransformer(L.LightningModule):
+    def __init__(
+            self,
+            n_vocab: int, max_len: int = 128, d_model: int = 128, pad_index: Optional[int] = None,
+            pos_enc: type[nn.Module] = PositionalEncoding, encoder: type[nn.Module] = TransformerEncoder,
+            n_blocks: int = 2, n_heads: int = 2, d_ff: int = 256, dropout: float = 0.1,
+            prenorm: bool = True, postnorm: bool = True, norm: type[nn.Module] = nn.LayerNorm,
+            activation: Callable[[torch.Tensor], torch.Tensor] = nn.ReLU(),
+            attn_drop: bool = True, ff_drop: bool = True
+    ):
+        super().__init__()
+        self.model = GenerativeTextTransformer(
+            n_vocab, max_len, d_model, pad_index=pad_index,
+            pos_enc=pos_enc, encoder=encoder,
+            n_blocks=n_blocks, n_heads=n_heads, d_ff=d_ff, dropout=dropout,
+            prenorm=prenorm, postnorm=postnorm, norm=norm,
+            activation=activation, attn_drop=attn_drop, ff_drop=ff_drop,
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+    @staticmethod
+    def loss(logits, targets):
+        return F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+
+    def configure_optimizers(self):
+        optimizer = optim.Adam(self.parameters(), lr=3e-4)
+        return optimizer
+
+    def _step(self, batch, name, **metrics):
+        x, targets = batch
+        logits = self(x)
+        loss = self.loss(logits, targets)
+        correct = (logits.detach().argmax(dim=2) == targets).sum().item()
+        accuracy = correct / targets.numel()
+        self.log_dict({f'{name}_loss': loss, f'{name}_accuracy': accuracy, **metrics}, prog_bar=True)
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self._step(batch, 'train')
+
+    def validation_step(self, batch, batch_idx):
+        return self._step(batch, 'val')
+
+    def test_step(self, batch, batch_idx):
+        return self._step(batch, 'test')
+
+    def predict_step(self, batch, batch_idx):
+        x, _ = batch
+        y = self(x)
         return y
