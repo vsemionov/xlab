5eac344f09931719d3e07af04ed2a0d539601dab
Victor Semionov <vsemionov@gmail.com>
2025-04-16 08:45:32 +0300

    retry download on error

--- xlabml/utils.py
+++ xlabml/utils.py
@@ -18,6 +18,8 @@ from typing import Optional, Union, Iterable
 import requests
 from tqdm.auto import tqdm
 from rich.progress import track
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
 
 def progress_bar(iterable: Iterable, kind: str = 'tqdm', total: Optional[float] = None, desc: str = 'Working'):
@@ -29,11 +31,59 @@ def progress_bar(iterable: Iterable, kind: str = 'tqdm', total: Optional[float] 
         assert False
 
 
+def requests_retry_session(
+        session=None,  # existing session to decorate
+        retries=3,  # number of total retries, excluding the initial attempt
+        connect=None,  # number of retries on connect errors, 0 or False to disable, None to use the total retries
+        read=None,  # number of retries on read errors, 0 or False to disable, None to use the total retries
+        redirect=None,  # max number of redirects (see below), 0 or False to disable, None to use the total retries
+        status=None,  # number of retries on bad status, 0 to disable, None to use the total retries
+        other=None,  # number of retries on other errors (possibly after data sent), 0 to disable, None to use total
+        allowed_methods=Retry.DEFAULT_ALLOWED_METHODS,  # limits read and status (but not connect/other) error retries
+        status_forcelist=(429, 500, 502, 503, 504),  # unconditionally retry on these statuses, besides default ones
+        backoff_factor=0.5  # first retry is immediate, second is after 2x this time
+):
+    # redirects are handled by the requests library and limited by Session.max_redirects
+    # the retry counters are reset for each redirect
+    # the Retry.redirect value has no effect
+
+    session = session or requests.Session()
+
+    session.max_redirects = retries if redirect is None else int(redirect)
+
+    prefixes = ('http://', 'https://')
+
+    for prefix, adapter in session.adapters.items():
+        if prefix in prefixes:
+            adapter.close()
+
+    for prefix in prefixes:
+        retry = Retry(
+            total=retries,
+            connect=connect,
+            read=read,
+            redirect=redirect,
+            status=status,
+            other=other,
+            allowed_methods=allowed_methods,
+            status_forcelist=status_forcelist,
+            backoff_factor=backoff_factor,
+            respect_retry_after_header=False  # prevent server-controlled delays
+        )
+
+        adapter = HTTPAdapter(max_retries=retry)
+
+        session.mount(prefix, adapter)
+
+    return session
+
+
 def download(url: str, save_path: Union[Path, str], desc: Optional[str] = None):
     block_size = 32 * 1024
     desc = desc or save_path.name
     Path(save_path).parent.mkdir(parents=True, exist_ok=True)
-    with requests.get(url, stream=True) as response:
+    with requests_retry_session(retries=3) as session, \
+            session.get(url, stream=True) as response:
         response.raise_for_status()
         total = int(response.headers.get('content-length', 0))
         with open(save_path, 'wb') as file, \
