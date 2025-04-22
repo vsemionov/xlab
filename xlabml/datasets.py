75c1bf4e2875a77f55fc7b95d625b8623464694f
Victor Semionov <vsemionov@gmail.com>
2025-04-22 18:55:24 +0300

    configurable indexed or materialized datasets

--- xlabml/datasets.py
+++ xlabml/datasets.py
@@ -172,7 +172,8 @@ class IndexedSequenceDataset(SequenceDataset):
         tokenizer = parent.tokenizer
         column = 'index'
         dataset = self._index(parent, column, seq_len, step_size, pad_incomplete, num_proc)
-        super(SequenceDataset, self).__init__(column=column, parent=parent, dataset=dataset, seq_len=seq_len)  # super^2
+        super(SequenceDataset, self).__init__(column=column, parent=parent, dataset=dataset)  # super-super
+        self.seq_len = seq_len
         self.sos = np.array([tokenizer[tokenizer.sos_token]])
         self.eos = np.array([tokenizer[tokenizer.eos_token]])
         self.padding = np.array([tokenizer[tokenizer.pad_token]]).repeat(seq_len)
@@ -254,7 +255,8 @@ class MaterializedSequenceDataset(SequenceDataset):
         pad_index = tokenizer[tokenizer.pad_token]
         dataset = self._generate(parent, column, seq_len, step_size, concatenate, pad_incomplete, train_sos, num_proc,
             sos_index, eos_index, pad_index)
-        super(SequenceDataset, self).__init__(column=column, parent=parent, dataset=dataset, seq_len=seq_len)  # super^2
+        super(SequenceDataset, self).__init__(column=column, parent=parent, dataset=dataset)  # super-super
+        self.seq_len = seq_len
         self.concatenate = concatenate
         self.train_sos = train_sos
         self.sos_index = sos_index
