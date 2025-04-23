44f32fc89758c6c2341763662a80d47710499b81
Victor Semionov <vsemionov@gmail.com>
2025-04-23 06:50:56 +0300

    cosine warmup scheduler

--- xlabml/sched.py
+++ xlabml/sched.py
@@ -14,7 +14,7 @@
 
 import sys
 import math
-from typing import Union
+from typing import Optional, Union
 
 import torch.optim.lr_scheduler as lr_scheduler
 
@@ -29,9 +29,22 @@ class XLabLRScheduler(lr_scheduler.LRScheduler):
 
 
 class CosineWarmupLR(lr_scheduler.LRScheduler):
-    def __init__(self, optimizer, total_iters: Union[int, float], warmup_iters: Union[int, float] = 0.1):
+    def __init__(
+            self,
+            optimizer,
+            total_iters: Union[int, float],
+            warmup_iters: Union[int, float] = 0.1,
+            start_factor: Optional[float] = None,
+            end_factor: Optional[float] = None,
+            exact: bool = True,
+    ):
+        assert type(total_iters) is int or total_iters == float('inf')
+        assert total_iters > warmup_iters + exact
         self.total_iters = total_iters
         self.warmup_iters = warmup_iters if isinstance(warmup_iters, int) else int(warmup_iters * total_iters)
+        self.start_factor = start_factor if start_factor is not None else 1 / (warmup_iters + 1)
+        self.end_factor = end_factor if end_factor is not None else 1e-4
+        self.exact = exact
         super().__init__(optimizer)
 
     def get_lr(self):
@@ -39,10 +52,11 @@ class CosineWarmupLR(lr_scheduler.LRScheduler):
         return [base_lr * lr_factor for base_lr in self.base_lrs]
 
     def get_lr_factor(self, epoch):
-        epoch = min(epoch, self.total_iters - 1)
+        epoch = min(epoch, self.total_iters - self.exact)
         if epoch < self.warmup_iters:
-            return (epoch + 1) / (self.warmup_iters + 1)
-        return 0.5 * (1 + math.cos(math.pi * (epoch - self.warmup_iters) / (self.total_iters - self.warmup_iters)))
+            return self.start_factor + (1 - self.start_factor) * epoch / self.warmup_iters
+        cos = math.cos(math.pi * (epoch - self.warmup_iters) / (self.total_iters - self.warmup_iters - self.exact))
+        return self.end_factor + 0.5 * (1 - self.end_factor) * (1 + cos)
 
 
 # Dynamically create subclasses of lr_scheduler.LRScheduler, accepting Lightning's additional configuration,
